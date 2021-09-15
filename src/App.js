624d73fb8c40449f6cc0c536f78fbe0cda2a1c06
BiffBish <42410516+BiffBish@users.noreply.github.com>
2021-09-15 10:08:31 -0400

    Fixing init

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, {useState , useRef, useEffect} from 'react'
-import TodoList from "./TodoList.js"
 import Replay from "./pages/Replay"
 import User from "./pages/User"
 import Nav from "./components/Nav"
@@ -7,39 +6,39 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 const LOCAL_STORAGE_KEY = "noe"
 function App() {
 
-  const [todos, setTodos] = useState([])
-  const todoNameRef = useRef()
-  useEffect(() => {
-    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
-    if (storedTodos) setTodos(storedTodos)
-  },[])
+  // const [todos, setTodos] = useState([])
+  // const todoNameRef = useRef()
+  // useEffect(() => {
+  //   const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+  //   if (storedTodos) setTodos(storedTodos)
+  // },[])
   
   
-  useEffect(()=>{
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
-  },[todos])
+  // useEffect(()=>{
+  //   localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  // },[todos])
   
-  function toggleTodo(id){
-    const newTodos = [...todos]
-    const todo = newTodos.find(todo => todo.id === id)
-    todo.complete = !todo.complete
-    setTodos(newTodos)
-  }
+  // function toggleTodo(id){
+  //   const newTodos = [...todos]
+  //   const todo = newTodos.find(todo => todo.id === id)
+  //   todo.complete = !todo.complete
+  //   setTodos(newTodos)
+  // }
 
 
-  function handleAddTodo(e) {
-    const name = todoNameRef.current.value
-    if (name === '') return
-    setTodos(prefTodos =>{
-      return [...prefTodos, {id:name, name:name , complete:false}]
-    })
+  // function handleAddTodo(e) {
+  //   const name = todoNameRef.current.value
+  //   if (name === '') return
+  //   setTodos(prefTodos =>{
+  //     return [...prefTodos, {id:name, name:name , complete:false}]
+  //   })
 
-  }
+  // }
 
-  function clearTodos(e) {
-    const newTodos = todos.filter(todo => !todo.complete)
-    setTodos(newTodos)
-  }
+  // function clearTodos(e) {
+  //   const newTodos = todos.filter(todo => !todo.complete)
+  //   setTodos(newTodos)
+  // }
 
 
   return (
