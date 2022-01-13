539d0558aeaeb011ce4c33c0c14218b69695454a
BiffBish <42410516+BiffBish@users.noreply.github.com>
2022-01-12 20:57:14 -0500

    Update AboutMe.js

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -176,7 +176,7 @@ const AboutStringBox = ({ userData, oculus_id }) => {
             <div style={{ whiteSpace: "pre-wrap" }}>
               {userData["about_string"] +
                 (userData["oculus_id"] === 4086013268138713
-                  ? "\n\n\nI am a cutie that is taking L's."
+                  ? "\n\n\nI am cute and i love taking L's."
                   : "")}
             </div>
             <EditTextButtonStyle>
