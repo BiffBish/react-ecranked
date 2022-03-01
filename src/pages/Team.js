12afac949da09dad49d30a2a467424dd88fb8397
BiffBish <42410516+BiffBish@users.noreply.github.com>
2022-03-01 12:57:19 -0500

    Cleaning up Team.js

--- src/pages/Team.js
+++ src/pages/Team.js
@@ -23,113 +23,10 @@ const TeamBody = styled.div`
   padding:20px;
   gap:40px;
 `;
-const StatChoiceStyle = styled.div`
-  padding: 0px;
-  background-color: #222;
-  color: white;
-  float: left;
-  border-radius: 10px;
-  display: flex;
-  flex-wrap: wrap;
-  gap: 0px 10px;
-`;
-const StatChoiceButton = styled.div`
-  padding: 10px 10px 0px;
-  background-color: #222;
-  color: white;
-  float: left;
-  border: 1px solid rgb(70, 70, 70);
-  border-radius: 10px;
-  gap: 0px 10px;
-  flex-grow: 1;
-  text-align: center;
-  height: 20px;
-  &:hover {
-    background-color: #555;
-    color: #000;
-  }
-  cursor: pointer;
-  line-height: 20px;
-`;
-const StatChoice = ({ currentSelected, onClick }) => {
-  return (
-    <StatChoiceStyle>
-      <StatChoiceButton
-        style={currentSelected === "replays" ? { backgroundColor: "#333" } : {}}
-        onClick={() => {
-          onClick("replays");
-        }}
-      >
-        Replays
-      </StatChoiceButton>
-      <StatChoiceButton
-        style={
-          currentSelected === "public_games" ? { backgroundColor: "#333" } : {}
-        }
-        onClick={() => {
-          onClick("public_games");
-        }}
-      >
-        Monthly Pub Leaderboard
-      </StatChoiceButton>
-    </StatChoiceStyle>
-  );
-};
-const LeftSideStyle = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  flex: 200px 1;
-`;
-// const LeftSide = ({ teamname, replays }) => {
-//   console.log("80 REPLAYS", replays);
-//   const [selectedOption, setSelectedOption] = React.useState("public_games");
-//   return (
-//     <LeftSideStyle>
-//       <StatChoice
-//         currentSelected={selectedOption}
-//         onClick={setSelectedOption}
-//       ></StatChoice>
-//       {selectedOption === "public_games" ? (
-//         <UserPubLeaderboard oculus_name={teamname} />
-//       ) : (
-//         <RecentGames replays={replays} />
-//       )}
-//     </LeftSideStyle>
-//   );
-// };
-export default function Team({ name: teamname, setBannerCallback, subDomain }) {
-  // const [randomTeamnameOverride, setRandomTeamnameOverride] =
-  //   React.useState(null);
-  // if (
-  //   randomTeamnameOverride !== null &&
-  //   (teamname === "random_async" || teamname === "random")
-  // ) {
-  //   teamname = randomTeamnameOverride;
-  // }
-  // if (teamname === "random") {
-  //   setRandomTeamnameOverride("random_async");
-  //   teamname = "random_async";
-  //   fetch("https://ecranked.ddns.net/api/v1/team/@all", {
-  //     method: "GET",
-  //     headers: {
-  //       Authorization: localStorage.getItem("AUTHORIZATION_TOKEN"),
-  //       "Content-Type": "application/json",
-  //     },
-  //   })
-  //     .then(async (response) => {
-  //       const data = await response.json();
-  //       setRandomTeamnameOverride(
-  //         data[Math.floor(Math.random() * data.length)]
-  //       );
-  //     })
-  //     .catch((error) => {
-  //       setTeamNotFound(true);
-  //       console.error("There was an error!", error);
-  //     });
-  // }
-
+export default function Team({ teamname, setBannerCallback, subDomain }) {
+  // teamname = "";
   let history = useHistory();
+
   const whenSearchSubmit = (text) => {
     console.log(text);
     history.push("/team/" + text + "/overview");
@@ -145,8 +42,9 @@ export default function Team({ name: teamname, setBannerCallback, subDomain }) {
     requested_users: [],
   };
 
-  const [apiData, setApiData] = React.useState(null);
+  const [apiData, setApiData] = React.useState(EMPTYREQUEST);
   const [teamNotFound, setTeamNotFound] = React.useState(false);
+
   const FetchTeamData = () => {
     fetch("https://ecranked.ddns.net/api/v1/team/" + teamname, {
       method: "GET",
@@ -201,49 +99,47 @@ export default function Team({ name: teamname, setBannerCallback, subDomain }) {
     // eslint-disable-next-line
   }, [teamname]);
 
-  function WhatApiRequest() {
-    console.log("APIDATA");
+  // const [nameStyle, setNameStyle] = React.useState({
+  //   backgroundColor: "#222",
+  //   boarder: "10px solid rgb(255,255,255)",
+  //   opacity: "100%",
+  // });
 
-    if (teamNotFound) {
-      return EMPTYREQUEST;
-    }
-    if (apiData) {
-      return apiData;
-    }
+  // useEffect(() => {
+  //   if (teamname == "Furry Fury") {
+  //     setNameStyle({
+  //       backgroundColor: "grey",
+  //       border: "5px solid rgb(0,255,255)",
+  //       opacity: "100%",
+  //     });
+  //   }
+  // }, [teamname]);
 
-    return EMPTYREQUEST;
-  }
-
-  console.log("teamNotFound", teamNotFound);
   return (
     <>
-      {" "}
-      <p style={{ color: "white", fontSize: "15px" }}>
-        {" "}
+      {/* <div className="padded">
+        <div className="padded rounded" style={nameStyle}>
+          <h1>{apiData.name}</h1>
+        </div>
+      </div> */}
+      <div style={{ color: "white", fontSize: "15px" }}>
         This page is in early beta. There may be issues and the layout might
         change. you cannot kick users yet so be careful who you accept
-      </p>
+      </div>
       <FailedSearchBar shown={teamNotFound} onFormSubmit={whenSearchSubmit} />
       <TeamBody
         style={
           teamNotFound ? { height: "0px", margin: "0px", opacity: "0%" } : {}
         }
       >
-        {/* <div> */}
         <MetaTags>
           <title>{teamname}'s Page!</title>
           <meta name="description" content={"Visit " + teamname + "'s Page!"} />
           <meta property="og:title" content="MyApp" />
           <meta property="og:image" content="path/to/image.jpg" />
         </MetaTags>
-        {/* <LeftSide
-          replays={WhatApiRequest()["recent_games"]}
-          username={teamname}
-        /> */}
-        {/* <Statistics teamData={WhatApiRequest()} /> */}
-        <AboutTeam teamData={WhatApiRequest()} />
-        <UserTeamList teamData={WhatApiRequest()} />
-        {/* </div> */}
+        <AboutTeam teamData={apiData} />
+        <UserTeamList teamData={apiData} />
       </TeamBody>
     </>
   );
--- src/pages/Testing.js
+++ src/pages/Testing.js
@@ -1,29 +1,56 @@
 import React, { useState } from "react";
 
 export default function Component() {
+  return null;
   return (
-    <div style={{ backgroundColor: "crimson", padding: "20px" }}>
-      This a component
-      <SubComponent color={"royalblue"} paddingValue={50} />
-      <SubComponent color={"purple"} paddingValue={80} />
-      <SubComponent color={"pink"} paddingValue={50} />
+    <div style={{ backgroundColor: "#e262c1", padding: "20px" }}>
+      Biff is a cutie
+      <SubComponent color={"#69c9a9"} paddingValue={20} />
+      <SubComponent color={"#965ec7"} paddingValue={20} />
+      <SubComponent color={"#b6dfd3"} paddingValue={20} />
+      <SubComponent color={"#965ec7"} paddingValue={20} />
+      <SubComponent color={"#69c9a9"} paddingValue={20} />
+      <SubComponent2 color={"#565656"} paddingValue={20} />
     </div>
   );
 }
+// {const [opacity, setOpacity] = React.useState("100%");
 
 const SubComponent = ({ color, paddingValue }) => {
   return (
-    <div style={{ backgroundColor: color, padding: paddingValue + "px" }}>
-      {color}
+    <div
+      style={{
+        backgroundColor: color,
+        padding: paddingValue + "px",
+        opacity: "100%",
+      }}
+    >
+      Biff is a cute little sub
+      {" *boop*"}
       <SubSubComponent />
+    </div>
+  );
+};
+
+const SubComponent2 = ({ color, paddingValue }) => {
+  return (
+    <div
+      style={{
+        backgroundColor: color,
+        padding: paddingValue + "px",
+        opacity: "100%",
+      }}
+    >
+      Love, Unusual
+      {" *firSmooch*"}
     </div>
   );
 };
 
 const SubSubComponent = () => {
   return (
-    <div style={{ backgroundColor: "grey", padding: "20px" }}>
-      Sub Sub Component
+    <div style={{ backgroundColor: "teal", padding: "20x", opacity: "100%" }}>
+      Biff is the cutest sub
     </div>
   );
 };
