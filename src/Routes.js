2b2ab9425c34b1bf45205ae74ff0cc2e66016749
BiffBish <42410516+BiffBish@users.noreply.github.com>
2022-04-11 18:29:26 -0400

    Update Routes.js

--- src/Routes.js
+++ src/Routes.js
@@ -59,41 +59,6 @@ const UserIcon = styled.img`
   height: 40px;
   margin: 20px;
 `;
-const openReticlePopupChecker = new Promise((resolve, reject) => {
-  var popupBlockerChecker = {
-    check: function (popup_window) {
-      var _scope = this;
-      if (popup_window) {
-        if (/chrome/.test(navigator.userAgent.toLowerCase())) {
-          setTimeout(function () {
-            _scope._is_popup_blocked(_scope, popup_window);
-          }, 200);
-        } else {
-          popup_window.onload = function () {
-            _scope._is_popup_blocked(_scope, popup_window);
-          };
-        }
-      } else {
-        _scope._displayError();
-        resolve(false);
-      }
-    },
-    _is_popup_blocked: function (scope, popup_window) {
-      if (popup_window.innerHeight > 0 == false) {
-        scope._displayError();
-        resolve(false);
-      }
-      resolve(true);
-    },
-    _displayError: function () {
-      alert(
-        "Popup Blocker is enabled! Please add this site to your exception list."
-      );
-    },
-  };
-  let popup = window.open("/reticle/dashboard", "", "popup");
-  popupBlockerChecker.check(popup);
-});
 function Routes() {
   const [globalUserState, setGlobalUserState] = useContext(GlobalUserState);
   // const [clientData, setClientData] = React.useState({
@@ -479,31 +444,76 @@ function Routes() {
         <Route
           exact
           path={`/reticle/dashboard/open`}
-          render={async () => {
-            //Check if user has popups enabled and if not prompt them to enable it
-            //asyncronsouly resolve the promise
-            const hasPopupsEnabled = await openReticlePopupChecker();
-            if (hasPopupsEnabled) {
-              window.opener = null;
-              window.open("", "_self");
-              window.close();
-              window.close();
-              return <Redirect to={"/"} />;
-            } else {
-              return (
-                <div>
-                  Popups are disabled. Please enable them and refresh the page.
-                </div>
-              );
-            }
-
-            //redirect home
+          render={() => {
+            console.log("[23] Open");
+            return <OasisDashboardPopup />;
           }}
         />
       </PageBody>
     </Router>
   );
 }
+
+const OasisDashboardPopup = ({}) => {
+  console.log("[23] Running useEffect");
+  const [isEnabled, setIsEnabled] = useState(true);
+
+  useEffect(() => {
+    async function getUser() {
+      const hasPopupsEnabled = await new Promise((resolve, reject) => {
+        console.log("Running Promise");
+        var popupBlockerChecker = {
+          check: function (popup_window) {
+            var _scope = this;
+            if (popup_window) {
+              if (/chrome/.test(navigator.userAgent.toLowerCase())) {
+                setTimeout(function () {
+                  resolve(_scope._is_popup_blocked(_scope, popup_window));
+                }, 200);
+              } else {
+                popup_window.onload = function () {
+                  resolve(_scope._is_popup_blocked(_scope, popup_window));
+                };
+              }
+            } else {
+              _scope._displayError();
+              resolve(false);
+            }
+          },
+          _is_popup_blocked: function (scope, popup_window) {
+            if (popup_window.innerHeight > 0 == false) {
+              scope._displayError();
+              return false;
+            }
+            return true;
+          },
+          _displayError: function () {
+            alert(
+              "Popup Blocker is enabled! Please add this site to your exception list."
+            );
+          },
+        };
+        let popup = window.open("/reticle/dashboard", "", "popup");
+        popupBlockerChecker.check(popup);
+      });
+      setIsEnabled(hasPopupsEnabled);
+      if (hasPopupsEnabled) {
+        window.opener = null;
+        window.open("", "_self");
+        window.close();
+        window.close();
+        return <Redirect to={"/"} />;
+      }
+    }
+    getUser();
+  }, []);
+  if (isEnabled) return null;
+  return (
+    <div className="rounded border padded centered" style={{ margin: "50px" }}>
+      <h1>Popups are disabled. Please enable them and refresh the page.</h1>
+    </div>
+  );
+};
 
 export default Routes;
 function DiscordOAuthCallback({ callbackCode, onFinish }) {
