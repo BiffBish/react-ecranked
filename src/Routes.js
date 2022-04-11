ca8bad81163fb9baeabf2031fb2b8c9ac8b0aacc
BiffBish <42410516+BiffBish@users.noreply.github.com>
2022-04-11 18:11:03 -0400

    Update Routes.js

--- src/Routes.js
+++ src/Routes.js
@@ -59,37 +59,41 @@ const UserIcon = styled.img`
   height: 40px;
   margin: 20px;
 `;
-var popupBlockerChecker = {
-  check: function (popup_window) {
-    var _scope = this;
-    if (popup_window) {
-      if (/chrome/.test(navigator.userAgent.toLowerCase())) {
-        setTimeout(function () {
-          _scope._is_popup_blocked(_scope, popup_window);
-        }, 200);
+const openReticlePopupChecker = new Promise((resolve, reject) => {
+  var popupBlockerChecker = {
+    check: function (popup_window) {
+      var _scope = this;
+      if (popup_window) {
+        if (/chrome/.test(navigator.userAgent.toLowerCase())) {
+          setTimeout(function () {
+            _scope._is_popup_blocked(_scope, popup_window);
+          }, 200);
+        } else {
+          popup_window.onload = function () {
+            _scope._is_popup_blocked(_scope, popup_window);
+          };
+        }
       } else {
-        popup_window.onload = function () {
-          return _scope._is_popup_blocked(_scope, popup_window);
-        };
+        _scope._displayError();
+        resolve(false);
       }
-    } else {
-      _scope._displayError();
-      return false;
-    }
-  },
-  _is_popup_blocked: function (scope, popup_window) {
-    if (popup_window.innerHeight > 0 == false) {
-      scope._displayError();
-      return false;
-    }
-    return true;
-  },
-  _displayError: function () {
-    alert(
-      "Popup Blocker is enabled! Please add this site to your exception list."
-    );
-  },
-};
+    },
+    _is_popup_blocked: function (scope, popup_window) {
+      if (popup_window.innerHeight > 0 == false) {
+        scope._displayError();
+        resolve(false);
+      }
+      resolve(true);
+    },
+    _displayError: function () {
+      alert(
+        "Popup Blocker is enabled! Please add this site to your exception list."
+      );
+    },
+  };
+  let popup = window.open("/reticle/dashboard", "", "popup");
+  popupBlockerChecker.check(popup);
+});
 function Routes() {
   const [globalUserState, setGlobalUserState] = useContext(GlobalUserState);
   // const [clientData, setClientData] = React.useState({
@@ -475,12 +479,11 @@ function Routes() {
         <Route
           exact
           path={`/reticle/dashboard/open`}
-          render={() => {
+          render={async () => {
             //Check if user has popups enabled and if not prompt them to enable it
-
-            let popup = window.open("/reticle/dashboard", "", "popup");
-            let result = popupBlockerChecker.check(popup);
-            if (result) {
+            //asyncronsouly resolve the promise
+            const hasPopupsEnabled = await openReticlePopupChecker();
+            if (hasPopupsEnabled) {
               window.opener = null;
               window.open("", "_self");
               window.close();
